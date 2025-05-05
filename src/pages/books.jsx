ad5d0d663dcad2f67ccc650e5720bfa0d5ef709f
林弘季 <hayashihiroki@hayashihiroshikinoMacBook-Air.local>
2025-05-05 15:52:18 +0900

    feature: Tailwind CSS/ books.jsx

--- src/pages/books.jsx
+++ src/pages/books.jsx
@@ -16,7 +16,7 @@ function Books() {
 					}
 				);
 				
-				setBooks(response.data);
+				setBooks(response.data.slice(0.10));
 				console.log('fetchBooks', books);
 			} catch (err) {
 				console.error('APIリクエスト中にエラー: ', err);
@@ -33,17 +33,18 @@ function Books() {
 	 },[]);
 
 	return (
-		<main>
-			<h2 className="text-3xl font-bold underline text-blue-600">書籍レビュー一覧</h2>
-			<ul>
+		<div className="max-w-3xl mx-auto p-4">
+			<h2 className="text-2xl font-bold mb-4 text-center">書籍レビュー一覧</h2>
+			{apiError && <p className="text-red-600">{apiError}</p>}
+			<ul className="space-y-4">
 				{books.map((book) => (
-					<li key={book.id} className='book'>
-						<h3 className='book-title'> {book.title}</h3>
-						<p>{book.reviewer}</p>
+					<li key={book.id} className="border p-4 shadow hover:shadow-md transition">
+						<h3 className="text-lg font-semibold text-blue-600"> {book.title}</h3>
+						<p className="text-sm text-gray-500 mt-2">投稿者：{book.reviewer}</p>
 					</li>
 				))}
 			</ul>
-		</main>
+		</div>
 	);
 }
 
