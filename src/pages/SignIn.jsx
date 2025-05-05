63bd3c9048c355e415f220d9e64d61cf0c1ab18c
林弘季 <hayashihiroki@hayashihiroshikinoMacBook-Air.local>
2025-05-05 13:49:02 +0900

    feature: SignIn/ API, RFM, errorUI

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,108 +1,79 @@
 import React, { useEffect, useState } from "react";
+import axios from 'axios';
 import { Link } from "react-router-dom";
+import { useForm } from 'react-hook-form';
 
 function SignIn () {
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const [emailError, setEmailError] = useState('');
-    const [passwordError, setPasswordError] = useState('');
-    const handleEmailChange = (e) => {
-        setEmail(e.target.value)
-        if(emailError) setEmailError(''); // 変更があったらエラークリア
-    }
-    const handlePasswordChange = (e) => {
-        setPassword(e.target.value);
-        if(passwordError) setPasswordError(''); // 変更があったらエラークリア
-    }
-    const onSignIn = (e) => {
-        e.preventDefault(); 
-        if(!email){
-            setEmailError('メールアドレスを入力してください');
-            return;
-        }
-        const eamilPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;    
-        if(!eamilPattern.test(email)){
-            setEmailError('メールアドレスの形式が不正です');
-            return;
-        }
-        if(!password){
-            setPasswordError('パスワードを入力してください');
-            return;
-        }
-
-        const formData = new FormData();
-        formData.append('name', name);
-        formData.append('email', email);
-        formData.append('password', password);
-        
-        // if (icon) {
-        //   formData.append('icon', icon);
-        // }
-
-        // try{
-        //   const userData = {
-        //     name: name,
-        //     email: email,
-        //     password: password,
-        //   }
-
-        //   const responseUser = await axios.post(
-        //     `${import.meta.env.VITE_API_URL}/users`,
-        //     userData,
-        //     {
-        //       headers: {
-        //         'Content-Type': 'application/json', /////この辺りNA
-        //       },
-        //     }
-        //   );
-        //   console.log('ユーザー情報送信結果: ', responseUser.data);
-        //   console.log('ユーザー情報送信結果、トークン: ', responseUser.data.token);  
-        // } catch(err){
-
-        // };
+    const { register, handleSubmit, formState: { errors }} = useForm();
+    const [apiError, setApiError] = useState('');
+    const onSubmit = async ( date ) => {
+        const { email, password } = date;
+    try {
+        const responseUser = await axios.post(
+            `${import.meta.env.VITE_API_URL}/signin`,
+            {email, password},
+            {
+              headers: {
+                'Content-Type': 'application/json',
+              },
+            }
+          );
+          console.log('ユーザー情報送信結果: ', responseUser.data);
+          console.log('ユーザー情報送信結果、トークン: ', responseUser.data.token);  
+          setApiError('');
+        } catch(err){
+            console.error('APIリクエスト中にエラー: ', err);
+            if (err.response) {
+                console.error('APIエラー内容:', err.response.data);
+                setApiError(`エラーが発生しました：${err.response.data.ErrorMessageJP}（ErrorCode: ${err.response.data.ErrorCode}）`);
+            } else {
+                console.error('通信エラー:', err.message);
+            };
+        };
     };
-
-    useEffect(() => {
-        console.log(emailError);
-    }, [emailError]);
-
-    useEffect(() => {
-        console.log(passwordError);
-    }, [passwordError]);
 
     return (
         <div>
             <main className="signin">
                 <h2>サインイン</h2>
-                <form className="signin-form">
+                <form className="signin-form" onSubmit={handleSubmit(onSubmit)}>
                 <label className="email-label">
                     メールアドレス
                     <input 
                         type="email"
                         className="email-input"
-                        value={email}
-                        required
-                        onChange={handleEmailChange}
+                        {...register('email', {
+                          required: 'メールアドレスを入力してください',
+                          pattern: {
+                            value: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
+                            message: 'メールアドレスの形式が不正です'
+                          }
+                        }
+                        )}
                     />
                 </label>
-                {emailError && <p className="error-message">{emailError}</p>}
+                {errors.email && <p className="error-message">{errors.email.message}</p>}
                 <br />
                 <label className="password-label">
                     パスワード
                     <input 
                         type="password"
                         className="password-input"
-                        value={password}
-                        required
-                        minLength="8"
-                        onChange={handlePasswordChange}
+                        {...register('password', {
+                          required: 'パスワードを入力してください',
+                          minLength: {
+                            value: 8,
+                            message: 'パスワードは8文字以上で入力してください'
+                          }
+                        }
+                        )}
                     />
                 </label>
-                {passwordError && <p className="error-message">{passwordError}</p>}
+                {errors.password && <p className="error-message">{errors.password.message}</p>}
                 < br />
-                <button type="submit" className="signin-button" onClick={onSignIn}>サインイン</button>
+                <button type="submit" className="signin-button">サインイン</button>
                 </form>
-
+                {apiError && <p className="error-message">{apiError}</p>}
                 <Link to="/signup">新規登録画面はこちら</Link>
             </main>
         </div>
